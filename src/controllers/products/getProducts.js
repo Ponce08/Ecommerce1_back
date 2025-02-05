b90ad707be39a2f62fbfd5a5f618388d7bee3cfd
ponce08 <yonathanponce2019@gmail.com>
2025-02-05 06:16:13 -0500

    use client supabase

--- src/controllers/products/getProducts.js
+++ src/controllers/products/getProducts.js
@@ -1,25 +1,27 @@
 import Product from '../../models/product.js';
-// import API from '../../../Api_Ecomm_Proyect1.json' assert { type: 'json' };
+import API from '../../../API_New.json' assert { type: 'json' };
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dm7llqul3',
+  api_key: '248591629323477',
+  api_secret: 'WTgofDsCcK0GM6uBdzXb6AmX2-c'
+});
 
 const getproducts = async (req, res) => {
   try {
-    const products = await Product.findAll();
-    if (products.length === 0) {
-      res.status(404).json({ message: 'Products is not found' });
+    // const products = await Product.findAll();
+    // if (products.length === 0) {
+    //   res.status(404).json({ message: 'Products is not found' });
+    // }
+
+    for (let i = 0; i < API.products.length; i++) {
+      await Product.create({
+        ...API.products[i]
+      });
     }
 
-    // let obj = {};
-    // for (let i = 0; i < API.products.length; i++) {
-    //   let priceMax = API.products[i].price;
-    //   if (obj[API.products[i].category]) {
-    //     if (obj[API.products[i].category] < priceMax) {
-    //       obj[API.products[i].category] = priceMax;
-    //     }
-    //   } else {
-    //     obj[API.products[i].category] = priceMax;
-    //   }
-    // }
-    res.status(200).json({ succefull: true, products });
+    res.status(200).json({ succefull: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/models/product.js
+++ src/models/product.js
@@ -20,10 +20,6 @@ const Product = sequelize.define(
       type: DataTypes.NUMERIC(10, 2),
       allowNull: true
     },
-    discountPercentage: {
-      type: DataTypes.NUMERIC(5, 2),
-      allowNull: false
-    },
     rating: {
       type: DataTypes.NUMERIC(3, 2),
       allowNull: false
@@ -34,63 +30,15 @@ const Product = sequelize.define(
     },
     brand: {
       type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    sku: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    weight: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    dimensions: {
-      type: DataTypes.JSON,
-      allowNull: false,
-      defaultValue: { width: 0, height: 0, depth: 0 }
-    },
-    warrantyInformation: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    shippingInformation: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    availabilityStatus: {
-      type: DataTypes.STRING(255),
-      allowNull: false
     },
     reviews: {
       type: DataTypes.JSON,
       allowNull: false,
       defaultValue: []
     },
-    returnPolicy: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    minimumOrderQuantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    meta: {
-      type: DataTypes.JSON,
-      allowNull: false,
-      defaultValue: {
-        createdAt: null,
-        updatedAt: null,
-        barcode: '',
-        qrCode: ''
-      }
-    },
     images: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: true
-    },
-    thumbnail: {
-      type: DataTypes.STRING(255),
-      allowNull: false
     }
   },
   {
